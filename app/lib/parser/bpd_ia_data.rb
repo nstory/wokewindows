0cbb5fb5aeabf325d2698cb94093786f05e766ac
Nathaniel Story <nathanielstory@gmail.com>
2020-06-15 20:46:10 -0400

    load ia data

---
+++ app/lib/parser/bpd_ia_data.rb
@@ -0,0 +1,65 @@
+class Parser::BpdIaData
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def records
+    page = []
+    lines.each do |line|
+      if /^(IA No.*First name|Last name.*number|Allega.*taken)$/ =~ line
+        page << []
+      else
+        page.last << line
+      end
+    end
+
+    (0 ... page[0].count).map do |i|
+      parse_first(page[0][i]).merge(parse_second(page[1][i])).merge(parse_third(page[2][i]))
+    end
+  end
+
+  private
+  def parse_first(line)
+    %r{^([^\s]+)\s+(\d+)?\s+(.+) (\d+/\d+/\d+)(.*)$}.match(line)
+    {
+      ia_no: $1,
+      case_no: $2 || "",
+      incident_type: $3.strip,
+      received_date: $4,
+      first_name: $5.strip
+    }
+  end
+
+  def parse_second(line)
+    badge_id_number = if /(\d+)$/.match(line)
+      $1
+    else
+      ""
+    end
+    split = line.split(/ {2,}/)
+    {
+      last_name: split[0].strip,
+      title: split[1].strip,
+      badge_id_number: badge_id_number
+    }
+  end
+
+  def parse_third(line)
+    hash = {allegation: "", finding: "", finding_date: ""}
+
+    if %r{\d{1,2}/\d{1,2}/\d{2,4}}.match(line)
+      hash[:finding_date] = $&
+    end
+
+    if %r{^(.*)  \b(Pending|Converted|Filed|Resolved|Not Sustained|Sustained|Unfounded|Exonerated|Withdrawn|Referred to District)\b}.match(line)
+      hash[:allegation] = $1
+      hash[:finding] = $2
+    end
+
+    hash.map { |k,v| [k, v.strip] }.to_h
+  end
+
+  def lines
+    IO.read(@filename).each_line.reject(&:blank?).map(&:strip)
+  end
+end
---
+++ spec/lib/parser/bpd_ia_data_spec.rb
@@ -0,0 +1,46 @@
+describe Parser::BpdIaData do
+  let(:file) { file_fixture("bpd_ia_data_2001_2011.txt") }
+  let(:records) { Parser::BpdIaData.new(file).records }
+
+  it "should parse first record" do
+    r = records.first
+    expect(r[:ia_no]).to eql("401")
+    expect(r[:case_no]).to eql("4192")
+    expect(r[:incident_type]).to eql("Internal investigation")
+    expect(r[:received_date]).to eql("1/2/2001")
+    expect(r[:first_name]).to eql("Mark Joseph")
+    expect(r[:last_name]).to eql("Loewen")
+    expect(r[:title]).to eql("Police Officer")
+    expect(r[:badge_id_number]).to eql("4528")
+    expect(r[:allegation]).to eql("Neg.Duty/Unreasonable Judge")
+    expect(r[:finding]).to eql("Sustained")
+    expect(r[:finding_date]).to eql("")
+  end
+
+  it "should parse last record" do
+    r = records.last
+    expect(r[:ia_no]).to eql("PRE2011-229")
+    expect(r[:incident_type]).to eql("Preliminary Investigation")
+    expect(r[:received_date]).to eql("9/29/2011")
+    expect(r[:first_name]).to eql("")
+    expect(r[:last_name]).to eql("Unknown")
+    expect(r[:title]).to eql("Police Officer")
+    expect(r[:badge_id_number]).to eql("")
+    expect(r[:allegation]).to eql("Neg.Duty/Unreasonable Judge")
+    expect(r[:finding]).to eql("Pending")
+    expect(r[:finding_date]).to eql("")
+  end
+
+  it "should parse a record" do
+    r = records.find { |r| r[:ia_no] == "P00101" }
+    expect(r[:case_no]).to eql("")
+    expect(r[:incident_type]).to eql("Inquiry")
+    expect(r[:received_date]).to eql("12/18/2001")
+  end
+
+  it "should parse finding_date" do
+    r = records.find { |r| r[:ia_no] == "9907" }
+    expect(r[:finding_date]).to eql("7/5/2007")
+    expect(r[:finding]).to eql("Not Sustained")
+  end
+end
