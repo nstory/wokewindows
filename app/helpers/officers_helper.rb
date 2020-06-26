e8815a7c669a901ef571656c3334cb39667d821d
Nathaniel Story <nathanielstory@gmail.com>
2020-06-25 20:36:24 -0400

    add neighborhood to officer show page

--- app/helpers/officers_helper.rb
+++ app/helpers/officers_helper.rb
@@ -1,2 +1,6 @@
 module OfficersHelper
+  def format_neighborhood(zip_code)
+    return format_unknown if !zip_code
+    "#{format_text(zip_code.neighborhood)}, #{format_text(zip_code.state)}"
+  end
 end
