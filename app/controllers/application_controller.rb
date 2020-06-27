0240ad34944b3d6f71edf09f262ac8fa6e188d28
Nathaniel Story <nathanielstory@gmail.com>
2020-06-27 07:08:09 -0400

    SITE_PASSWORD env var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  if Rails.configuration.site_password
+    http_basic_authenticate_with name: "anonymous", password: Rails.configuration.site_password
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Copsonrails
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.site_password = ENV['SITE_PASSWORD']
   end
 end
