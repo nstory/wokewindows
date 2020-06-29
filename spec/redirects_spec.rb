152e22c15e40e35ed3d9a4f99f139b40041dbfba
Nathaniel Story <nathanielstory@gmail.com>
2020-06-28 20:11:18 -0400

    redirect test!

--- spec/redirects_spec.rb
+++ spec/redirects_spec.rb
@@ -2,10 +2,8 @@ describe "redirects" do
   ["http://wokewindows.com", "http://wokewindows.org", "https://wokewindows.herokuapp.com"].each do |url|
     it "redirect #{url}" do
       response = Net::HTTP.get_response(URI(url))
-      expect(response.code).to eql("302")
-      expect(response.to_hash).to include(
-        "location" => ["https://www.wokewindows.org"]
-      )
+      expect(response.code).to match(/^3/)
+      expect(response.to_hash["location"].first).to match(%r{^https://www\.wokewindows\.org/?$})
     end
   end
 end
