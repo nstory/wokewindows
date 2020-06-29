ea9eae605fab250ee14d6afd42504e0d2bf5a8ba
Nathaniel Story <nathanielstory@gmail.com>
2020-06-29 14:25:49 -0400

    temporarily disable the download button

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -4,21 +4,31 @@ import { Controller } from "stimulus";
 const DEBOUNCE_TIME_MS = 500;
 
 export default class TableController extends Controller {
-  static targets = ["searchInput"];
+  static targets = ["searchInput", "downloadButton"];
 
   initialize() {
     this._performSearch = debounce(this._performSearch, DEBOUNCE_TIME_MS);
   }
 
   download(ev) {
+    // get CSV url and params from the datatable
     const url = this._dataTable().ajax.url().replace(/\.json$/, ".csv")
     const params = this._dataTable().ajax.params();
+
+    // temporarily disable the download button so user doesn't accidentally DOS us
+    this.downloadButtonTarget.disabled = true;
+    setTimeout(() => this.downloadButtonTarget.disabled = false, 5000);
+
+    // send the user to the CSV file; this will cause the browser to download it
     $.redirect(url, params, "POST");
   }
 
   search(ev) {
     const query = this.searchInputTarget.value;
+
+    // show the loading indicator over the table
     this._$wrapper().addClass("search-queued");
+
     this._performSearch(query);
   }
 
