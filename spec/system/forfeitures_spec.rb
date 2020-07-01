6b6a85a57b55cce1e448ef7c7efa0aa804fe4492
Nathaniel Story <nathanielstory@gmail.com>
2020-07-01 14:35:51 -0400

    look up forfeiture on masscourts.org

--- spec/system/forfeitures_spec.rb
+++ spec/system/forfeitures_spec.rb
@@ -1,11 +1,11 @@
 describe "Forfeitures" do
   let!(:incident) { Incident.create(incident_number: 42) }
-  let!(:forfeiture) { Forfeiture.create(sucv: "123-ABC", amount: "42.23", date: "2012-11-20", motor_vehicle: "2004 Dodge Neon", forfeitures_incidents: [ForfeituresIncident.new({incident: incident, incident_number: "123"})]) }
+  let!(:forfeiture) { Forfeiture.create(sucv: "2015-1234", amount: "42.23", date: "2012-11-20", motor_vehicle: "2004 Dodge Neon", forfeitures_incidents: [ForfeituresIncident.new({incident: incident, incident_number: "123"})]) }
 
   describe "index" do
     it "should display the forfeiture" do
       visit forfeitures_path
-      expect(page).to have_selector("td", text: "123-ABC")
+      expect(page).to have_selector("td", text: "2015-1234")
       expect(page).to have_selector("td", text: "$42.23")
       expect(page).to have_selector("td", text: "Nov 20, 2012")
       expect(page).to have_selector("td", text: "2004 Dodge Neon")
@@ -17,7 +17,8 @@ describe "Forfeitures" do
     before { driven_by(:rack_test) }
     it "should display the forfeiture" do
       visit forfeiture_path(forfeiture)
-      expect(page).to have_content("123-ABC")
+      expect(page).to have_content("2015-1234")
+      expect(page).to have_content("1584CV01234")
       expect(page).to have_content("$42.23")
       expect(page).to have_content("Nov 20, 2012")
       expect(page).to have_content("2004 Dodge Neon")
