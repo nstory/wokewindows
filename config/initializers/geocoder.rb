03207b4b236f01c7e5ec36490fa6be9def319600
Nathaniel Story <nathanielstory@gmail.com>
2020-07-10 13:14:05 -0400

    increase geocoder timeout to 10

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,6 @@
 Geocoder.configure(
   http_headers: { "User-Agent" => "Woke Windows Project n" + "s" + "tory" + "@wokewindows.org" },
+  timeout: 10,
   geocoder_ca: {
     api_key: ENV['GEOCODER_CA_API_KEY']
   }
