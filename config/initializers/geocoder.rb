83e6b711b003ebe41a3227baecea997fb7dce459
Nathaniel Story <nathanielstory@gmail.com>
2020-07-10 13:16:05 -0400

    ...

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,5 @@
 Geocoder.configure(
-  http_headers: { "User-Agent" => "Woke Windows Project n" + "s" + "tory" + "@wokewindows.org" },
+  http_headers: {"User-Agent" => ENV['GEOCODER_USER_AGENT']},
   timeout: 10,
   geocoder_ca: {
     api_key: ENV['GEOCODER_CA_API_KEY']
