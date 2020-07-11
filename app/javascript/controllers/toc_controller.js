2a3ecb4b1fb22dd8f6b2c6c85fcd63a1de0182e3
Nathaniel Story <nathanielstory@gmail.com>
2020-07-11 11:15:04 -0400

    toc_controller

---
+++ app/javascript/controllers/toc_controller.js
@@ -0,0 +1,27 @@
+import uniqueId from "lodash/uniqueId";
+import {Controller} from "stimulus";
+import tocbot from "tocbot";
+
+export default class TocController extends Controller {
+  connect() {
+    // need to pass a selector to tocbot, so give the element a unique id
+    // so it will have a unique selector
+    const id = uniqueId("toc_controller");
+    this.element.id = id;
+
+    tocbot.init({
+      tocSelector: `#${id}`,
+      hasInnerContainers: true,
+      scrollSmooth: false,
+      collapseDepth: 6
+    });
+  }
+
+  disconnect() {
+    // tell tocbot to remove its event listeners (including scroll listiners on
+    // document); skipRendering means it doesn't empty the toc div; turbolinks
+    // removes all the html on the page, so, no need
+    tocbot.options.skipRendering = true;
+    tocbot.destroy();
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,6 @@ require("shared/tables/incidents");
 require("shared/tables/officers");
 require("shared/tables/swats");
 require("shared/charts/compensations");
-require("toc");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
