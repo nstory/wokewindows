810cf13b0c3c418d6f7299ac00d228ded11e61a5
Nathaniel Story <nathanielstory@gmail.com>
2020-07-23 10:56:45 -0400

    scripts/daily_backup.sh

---
+++ scripts/daily_backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# daily database backup job
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+
+pg_dump "$DATABASE_URL" | gzip > wokewindows_db_$(date -I).sql.gz
+aws s3 cp wokewindows_db_*.gz 's3://wokewindows-backup/'
