dea8027c1ad3ba12dac20155157c001a25de6bd2
Nathaniel Story <nathanielstory@gmail.com>
2020-10-07 18:48:31 -0400

    ...

--- scripts/load_all_ia_cases.sh
+++ scripts/load_all_ia_cases.sh
@@ -6,6 +6,7 @@ mkdir -p data
 aws s3 cp 's3://wokewindows-data/iad_cases_2010-01-01_to_2020-08-15.csv.gz' data/
 aws s3 cp 's3://wokewindows-data/2010_to_2020_allegations.csv.gz' data/
 aws s3 cp 's3://wokewindows-data/bpd_ia_data_2001_2011.txt' data/
+aws s3 cp 's3://wokewindows-data/2014_Officer__IA_Log_redacted.csv.gz' data/
 
 rails r 'ComplaintOfficer.delete_all'
 rails r 'Complaint.delete_all'
