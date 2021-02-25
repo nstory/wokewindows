5806e633eb1c0b7218e3653267fb0301b9f4de0b
Nathaniel Story <nathanielstory@gmail.com>
2021-02-25 09:09:54 -0500

    less in daily.sh

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -9,15 +9,15 @@ IFS=$'\n\t'
 rails 'ckan:archive[public-works-active-work-zones,Public Works Active Work Zones,public_works_active_work_zones]'
 
 # download and import incident report data
-rails 'ckan:archive[crime-incident-reports-august-2015-to-date-source-new-system,Crime Incident Reports (August 2015 - To Date) (Source - New System),crime_incident_reports]'
-rails 'journals:download'
-rails 'importers:run'
-rails r 'Populater::IncidentsGeocode.populate'
+# rails 'ckan:archive[crime-incident-reports-august-2015-to-date-source-new-system,Crime Incident Reports (August 2015 - To Date) (Source - New System),crime_incident_reports]'
+# rails 'journals:download'
+# rails 'importers:run'
+# rails r 'Populater::IncidentsGeocode.populate'
 
 # download and import bpd news articles
-rails 'articles:download'
-rails r 'Importer::Article::BpdNews.import_all'
-rails r 'Populater::ArticlesOfficers.populate'
+# rails 'articles:download'
+# rails r 'Importer::Article::BpdNews.import_all'
+# rails r 'Populater::ArticlesOfficers.populate'
 
 # update counter_culture counters
 rails 'counters:fix'
